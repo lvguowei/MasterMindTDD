8d0f621ad9acfa0bd05dab2fabd3b03d25a7ed2b
lvguowei <lvguowei1002@gmail.com>
2016-11-27 09:47:04 +0200

    test only one guess valid return that one

--- src/test/GuesserTest.java
+++ src/test/GuesserTest.java
@@ -6,6 +6,7 @@ import com.guowei.lv.gameplay.Score;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class GuesserTest {
@@ -22,7 +23,6 @@ public class GuesserTest {
 
             @Override
             public void addScore(String guess, Score score) {
-
             }
         };
     }
@@ -31,5 +31,31 @@ public class GuesserTest {
     public void whenNoGuessesAreValid_nextGuessIsNil() throws Exception {
         GameEngine.Guesser guesser = new GameEngine.Guesser(failingChecker);
         assertNull(guesser.getNextGuess());
+    }
+
+    @Test
+    public void whenOnlyOneGuessIsValid_nextGuessIsThatOne() throws Exception {
+        GameEngine.Guesser guesser = new GameEngine.Guesser(new SingleChecker("BEEF"));
+        assertEquals("BEEF", guesser.getNextGuess());
+        assertNull(guesser.getNextGuess());
+    }
+}
+
+class SingleChecker implements GuessChecker {
+
+    private String validGuess;
+
+    SingleChecker(String validGuess) {
+        this.validGuess = validGuess;
+    }
+
+    @Override
+    public boolean shouldTry(String guess) {
+        return guess.equals(validGuess);
+    }
+
+    @Override
+    public void addScore(String guess, Score score) {
+
     }
 }
